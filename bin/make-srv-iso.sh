a25da4a2ef803a4c407655b872178a413a720b72
Henrik Andreasson <henrik.andreasson@cgi.com>
2022-11-21 21:36:48 +0100

    add support for configdir

--- bin/make-srv-iso.sh
+++ bin/make-srv-iso.sh
@@ -4,6 +4,7 @@ date_ts=$(date +"%Y-%m-%d_%H.%M.%S")
 logfile="$(mktemp -p logs logfile-${date_ts}-XXXXXXX)"
 isoname=""
 serverdir=""
+configdir=""
 
 while getopts n:s: flag; do
   case $flag in
@@ -11,6 +12,11 @@ while getopts n:s: flag; do
       ;;
     s) serverdir=$OPTARG;
       ;;
+    c) configdir=$OPTARG;
+      ;;
+    c) outputdir=$OPTARG;
+      ;;
+
     ?)
       exit;
       ;;
@@ -30,6 +36,15 @@ if [[ ! -d "$serverdir" ]] ; then
   exit -1
 fi
 
+if [ "x$configdir" == "x" ] ; then
+  configdir="configs"
+fi
+
+if [[ ! -d "$configdir" ]] ; then
+  echo "configdir not found ($configdir)"
+  exit -1
+fi
+
 output="custom-debian-iso-${isoname}-11.0.0-amd64.iso"
 
 /bin/echo -n "Creating the postintall deb..."
@@ -43,58 +58,61 @@ cp debian-fat-postinstall*_amd64.deb repos/postinstall/current/
 )
 
 /bin/echo -n "Updating common repos on the iso..."
-./bin/copy-repos.sh configs/repos.json "${serverdir}" >> "${logfile}" 2>&1
+./bin/copy-repos.sh "${configdir}/repos.json" "${serverdir}" >> "${logfile}" 2>&1
 cp lib/isolinux.cfg "${serverdir}/isolinux/"
 cp lib/csws.cfg "${serverdir}/isolinux/"
 /bin/echo  "done."
 
 /bin/echo -n  "Adding servers..."
-for server in configs/*server.json ; do
+for server in ${configdir}/*server.json ; do
 	servername=$(jq .hostname $server  | tr -d '"')
   packages=""
-	if [ -f "configs/$servername-packages.json" ] ; then
-    echo "Using configs/$servername-packages.json"
-		packages="configs/$servername-packages.json"
+	if [ -f "${configdir}/$servername-packages.json" ] ; then
+    echo "Using ${configdir}/$servername-packages.json"
+		packages="${configdir}/$servername-packages.json"
 	else
-    echo "Using configs/default-packages.json ($servername)"
-		packages="configs/default-packages.json"
+    echo "Using ${configdir}/default-packages.json ($servername)"
+		packages="${configdir}/default-packages.json"
 	fi
   support=""
-  if [ -f "configs/$servername-support.json" ] ; then
-		support="configs/$servername-support.json"
+  if [ -f "${configdir}/$servername-support.json" ] ; then
+		support="${configdir}/$servername-support.json"
 	else
-		support="configs/default-support.json"
+		support="${configdir}/default-support.json"
 	fi
+  if [ ! -f "${configdir}/$servername-network.json" ] ; then
+    continue
+  fi
 
-	if [ ! -f "configs/${servername}-network.json" ] ; then
-		echo "network file must exit: configs/${servername}-network.json"
+	if [ ! -f "${configdir}/${servername}-network.json" ] ; then
+		echo "network file must exit: ${configdir}/${servername}-network.json"
 		exit
 	fi
 	python3 ./lib/template-to-preseed.py --packages "$packages" \
     --server "$server" \
     --support "$support" \
-    --network "configs/${servername}-network.json" \
+    --network "${configdir}/${servername}-network.json" \
     > "${serverdir}/isolinux/preseed-$servername.cfg"
 
 	echo "added server: $servername"
 
   /bin/echo -n "Updating the iso with server repos..."
-  if [ -f "configs/${servername}-repos.json" ] ; then
-    ./bin/copy-repos.sh "configs/${servername}-repos.json" "${serverdir}" >> "${logfile}" 2>&1
-  elif [ -f "configs/default--repos.json" ] ; then
-    ./bin/copy-repos.sh "configs/default-repos.json" "${serverdir}" >> "${logfile}" 2>&1
+  if [ -f "${configdir}/${servername}-repos.json" ] ; then
+    ./bin/copy-repos.sh "${configdir}/${servername}-repos.json" "${serverdir}" >> "${logfile}" 2>&1
+  elif [ -f "${configdir}/default--repos.json" ] ; then
+    ./bin/copy-repos.sh "${configdir}/default-repos.json" "${serverdir}" >> "${logfile}" 2>&1
   else
-    echo "no repos config found, not: configs/${servername}-repos.json nor: configs/default-repos.json"
+    echo "no repos config found, not: ${configdir}/${servername}-repos.json nor: ${configdir}/default-repos.json"
   fi
   /bin/echo  "done."
 
 done
 
-./bin/create-isolinux-menu.sh
+./bin/create-isolinux-menu.sh "${serverdir}/isolinux"
 ./bin/create-uefi-menu.sh -i "${serverdir}/isolinux" -u "${serverdir}/boot/grub"
 
 /bin/echo -n "Creating the iso..."
-./bin/create-iso.sh -i "${isoname}" >> "${logfile}" 2>&1
+./bin/create-iso.sh -i "${isoname}" -s "$serverdir" >> "${logfile}" 2>&1
 /bin/echo  "done."
 
 echo "Iso: ${isoname} is in output: ${output}"
